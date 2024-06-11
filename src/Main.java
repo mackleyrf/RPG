34371739ebeccd1a7cf6af2b1ba1bb952e178cd3
Mackley Freire <mackleyrf@hotmail.com>
2024-06-11 15:42:24 -0300

    First character

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Character myCharacter = new Character();
+        myCharacter.name = "Mackley Freire";
+        myCharacter.level = 8;
+        myCharacter.profession = "Warrior";
+        myCharacter.health = 10;
+        myCharacter.strenght = 10;
+        myCharacter.agility = 10;
+        myCharacter.weapon = "Small Sword";
+        myCharacter.armor = "Leather Armor";
+        myCharacter.shield = "Wooden Shield";
+    }
+}
